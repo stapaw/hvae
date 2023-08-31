9699a4f9481fd9b9c4cc912a1ed985c5238d409a
Sebastian <sebastian.dziadzio@gmail.com>
2023-08-31 18:35:36 +0200

    Run hvae for longer and without additional blocks

--- hvae/models/blocks.py
+++ hvae/models/blocks.py
@@ -20,8 +20,8 @@ class Encoder(nn.Module):
         channels = [in_channels] + channels
         module = [
             nn.Sequential(
-                nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1),
-                activation(),
+                # nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1),
+                # activation(),
                 nn.Conv2d(
                     in_channels,
                     out_channels,
@@ -62,8 +62,8 @@ class Decoder(nn.Module):
         super().__init__()
         modules = [
             nn.Sequential(
-                nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1),
-                activation(),
+                # nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1),
+                # activation(),
                 nn.ConvTranspose2d(
                     in_channels,
                     out_channels,
