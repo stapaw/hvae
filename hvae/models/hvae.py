70d0c7aeb8dd57d606dcc919b0b52625df3ea898
Sebastian <sebastian.dziadzio@gmail.com>
2023-09-06 16:46:54 +0200

    Refactor to avoid code duplication

--- hvae/models/hvae.py
+++ hvae/models/hvae.py
@@ -75,7 +75,7 @@ class HVAE(VAE):
         h_1 = self.nn_z_1(z_2)
         mu_1, log_var_1 = torch.chunk(h_1, 2, dim=1)
         z_1 = self.reparameterize(mu_1 + delta_mu_1, log_var_1 + delta_log_var_1)
-
+        z_1 = self.decoder_input(z_1)
         x_hat = self.decode(z_1)
 
         return {
@@ -85,6 +85,8 @@ class HVAE(VAE):
             "log_var_1": log_var_1,
             "delta_mu_2": delta_mu_2,
             "delta_log_var_2": delta_log_var_2,
+            "z_1": z_1,
+            "z_2": z_2,
         }
 
     def loss_function(
@@ -142,6 +144,7 @@ class HVAE(VAE):
         h_1 = self.nn_z_1(z_2)
         mu_1, log_var_1 = torch.chunk(h_1, 2, dim=1)
         z_1 = self.reparameterize(mu_1, log_var_1)
+        z_1 = self.decoder_input(z_1)
         return self.decode(z_1)
 
 
@@ -165,64 +168,42 @@ class DCTHVAE(HVAE):
         Returns:
             List of tensors [reconstructed input, latent mean, latent log variance]
         """
-        r_1 = self.encoder(x).flatten(start_dim=1)  # add an MLP between enc and r_1?
-        r_2 = self.nn_r_2(r_1)
-
-        delta_1 = self.nn_delta_1(r_1)
-        delta_mu_1, delta_log_var_1 = torch.chunk(delta_1, 2, dim=1)
-        delta_log_var_1 = F.hardtanh(delta_log_var_1, -7.0, 2.0)
-
-        delta_2 = self.nn_delta_2(r_2)
-        delta_mu_2, delta_log_var_2 = torch.chunk(delta_2, 2, dim=1)
-        delta_log_var_2 = F.hardtanh(delta_log_var_2, -7.0, 2.0)
-        z_2 = self.reparameterize(delta_mu_2, delta_log_var_2)
-
-        h_1 = self.nn_z_1(z_2)
-        mu_1, log_var_1 = torch.chunk(h_1, 2, dim=1)
-        z_1 = self.reparameterize(mu_1 + delta_mu_1, log_var_1 + delta_log_var_1)
-
-        z_1 = self.decoder_input(z_1)
-        x_hat = self.decode(z_1)
-
-        z_2 = self.decoder_input_dct(z_2)
+        outputs = super().forward(x, y)
+        z_2 = self.decoder_input_dct(outputs["z2"])
         x_hat_dct = self.decode(z_2)
+        outputs["x_hat_dct"] = x_hat_dct
+        return outputs
 
-        return {
-            "x_hat": x_hat,
-            "x_hat_dct": x_hat_dct,
-            "delta_mu_1": delta_mu_1,
-            "delta_log_var_1": delta_log_var_1,
-            "log_var_1": log_var_1,
-            "delta_mu_2": delta_mu_2,
-            "delta_log_var_2": delta_log_var_2,
-        }
+        # r_1 = self.encoder(x).flatten(start_dim=1)  # add an MLP between enc and r_1?
+        # r_2 = self.nn_r_2(r_1)
 
-    def decode(self, z: Tensor) -> Tensor:
-        """Given a latent vector z, return the corresponding image.
-        Args:
-            z: Latent vector of shape (B x D)
-        Returns:
-            Tensor of shape (B x C x H x W)
-        """
-        z = z.view(
-            -1,
-            self.channels[-1],
-            self.encoder_output_img_size,
-            self.encoder_output_img_size,
-        )
-        return self.decoder(z)
+        # delta_1 = self.nn_delta_1(r_1)
+        # delta_mu_1, delta_log_var_1 = torch.chunk(delta_1, 2, dim=1)
+        # delta_log_var_1 = F.hardtanh(delta_log_var_1, -7.0, 2.0)
 
-    def loss_function(
-        self,
-        x: Tensor,
-        x_hat: Tensor,
-        x_hat_dct: Tensor,
-        delta_mu_1: Tensor,
-        delta_log_var_1: Tensor,
-        log_var_1: Tensor,
-        delta_mu_2: Tensor,
-        delta_log_var_2: Tensor,
-    ) -> dict:
+        # delta_2 = self.nn_delta_2(r_2)
+        # delta_mu_2, delta_log_var_2 = torch.chunk(delta_2, 2, dim=1)
+        # delta_log_var_2 = F.hardtanh(delta_log_var_2, -7.0, 2.0)
+        # z_2 = self.reparameterize(delta_mu_2, delta_log_var_2)
+
+        # h_1 = self.nn_z_1(z_2)
+        # mu_1, log_var_1 = torch.chunk(h_1, 2, dim=1)
+        # z_1 = self.reparameterize(mu_1 + delta_mu_1, log_var_1 + delta_log_var_1)
+
+        # z_1 = self.decoder_input(z_1)
+        # x_hat = self.decode(z_1)
+
+        # return {
+        #    "x_hat": x_hat,
+        #    "x_hat_dct": x_hat_dct,
+        #    "delta_mu_1": delta_mu_1,
+        #    "delta_log_var_1": delta_log_var_1,
+        #    "log_var_1": log_var_1,
+        #    "delta_mu_2": delta_mu_2,
+        #    "delta_log_var_2": delta_log_var_2,
+        # }
+
+    def loss_function(self, **kwargs) -> dict:
         """Compute the loss given ground truth images and their reconstructions.
         Args:
             x: Ground truth images of shape (B x C x H x W)
@@ -233,30 +214,39 @@ class DCTHVAE(HVAE):
         Returns:
             Dictionary containing the loss value and the individual losses
         """
-        x_dct = reconstruct_dct(x, k=self.k).to(self.device)
-        reconstruction_loss = F.mse_loss(x_hat, x, reduction="sum") / x.shape[0]
+        loss = super().loss_function(**kwargs)
+        x_hat_dct = kwargs["x_hat_dct"]
+        x_dct = reconstruct_dct(kwargs["x"], k=self.k).to(self.device)
         reconstruction_loss_dct = (
             F.mse_loss(x_hat_dct, x_dct, reduction="sum") / x_dct.shape[0]
         )
+        loss["reconstruction_loss_dct"] = reconstruction_loss_dct
+        loss["loss"] += reconstruction_loss_dct
+        return loss
+        # x_dct = reconstruct_dct(x, k=self.k).to(self.device)
+        # reconstruction_loss = F.mse_loss(x_hat, x, reduction="sum") / x.shape[0]
+        # reconstruction_loss_dct = (
+        #    F.mse_loss(x_hat_dct, x_dct, reduction="sum") / x_dct.shape[0]
+        # )
 
-        kl_divergence_z1 = 0.5 * (
-            delta_mu_1**2 / torch.exp(log_var_1)
-            + torch.exp(delta_log_var_1)
-            - delta_log_var_1
-            - 1
-        ).sum(-1)
+        # kl_divergence_z1 = 0.5 * (
+        #    delta_mu_1**2 / torch.exp(log_var_1)
+        #    + torch.exp(delta_log_var_1)
+        #    - delta_log_var_1
+        #    - 1
+        # ).sum(-1)
 
-        kl_divergence_z2 = 0.5 * (
-            delta_mu_2**2 + torch.exp(delta_log_var_2) - delta_log_var_2 - 1
-        ).sum(-1)
+        # kl_divergence_z2 = 0.5 * (
+        #    delta_mu_2**2 + torch.exp(delta_log_var_2) - delta_log_var_2 - 1
+        # ).sum(-1)
 
-        kl_divergence = kl_divergence_z1.mean() + kl_divergence_z2.mean()
+        # kl_divergence = kl_divergence_z1.mean() + kl_divergence_z2.mean()
 
-        loss = reconstruction_loss + reconstruction_loss_dct + self.beta * kl_divergence
+        # loss = reconstruction_loss + reconstruction_loss_dct + self.beta * kl_divergence
 
-        return {
-            "loss": loss,
-            "reconstruction_loss": reconstruction_loss,
-            "reconstruction_loss_dct": reconstruction_loss_dct,
-            "kl_divergence": kl_divergence,
-        }
+        # return {
+        #    "loss": loss,
+        #    "reconstruction_loss": reconstruction_loss,
+        #    "reconstruction_loss_dct": reconstruction_loss_dct,
+        #    "kl_divergence": kl_divergence,
+        # }
