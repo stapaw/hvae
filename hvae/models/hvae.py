de9c540e8d1f775c2e5d8cee1a5d72f02741603d
Sebastian <sebastian.dziadzio@gmail.com>
2023-09-07 17:49:43 +0200

    Remove commented out code

--- hvae/models/hvae.py
+++ hvae/models/hvae.py
@@ -178,35 +178,6 @@ class DCTHVAE(HVAE):
         outputs["x_hat_dct"] = x_hat_dct
         return outputs
 
-        # r_1 = self.encoder(x).flatten(start_dim=1)  # add an MLP between enc and r_1?
-        # r_2 = self.nn_r_2(r_1)
-
-        # delta_1 = self.nn_delta_1(r_1)
-        # delta_mu_1, delta_log_var_1 = torch.chunk(delta_1, 2, dim=1)
-        # delta_log_var_1 = F.hardtanh(delta_log_var_1, -7.0, 2.0)
-
-        # delta_2 = self.nn_delta_2(r_2)
-        # delta_mu_2, delta_log_var_2 = torch.chunk(delta_2, 2, dim=1)
-        # delta_log_var_2 = F.hardtanh(delta_log_var_2, -7.0, 2.0)
-        # z_2 = self.reparameterize(delta_mu_2, delta_log_var_2)
-
-        # h_1 = self.nn_z_1(z_2)
-        # mu_1, log_var_1 = torch.chunk(h_1, 2, dim=1)
-        # z_1 = self.reparameterize(mu_1 + delta_mu_1, log_var_1 + delta_log_var_1)
-
-        # z_1 = self.decoder_input(z_1)
-        # x_hat = self.decode(z_1)
-
-        # return {
-        #    "x_hat": x_hat,
-        #    "x_hat_dct": x_hat_dct,
-        #    "delta_mu_1": delta_mu_1,
-        #    "delta_log_var_1": delta_log_var_1,
-        #    "log_var_1": log_var_1,
-        #    "delta_mu_2": delta_mu_2,
-        #    "delta_log_var_2": delta_log_var_2,
-        # }
-
     def loss_function(self, **kwargs) -> dict:
         """Compute the loss given ground truth images and their reconstructions.
         Args:
@@ -227,30 +198,3 @@ class DCTHVAE(HVAE):
         loss["reconstruction_loss_dct"] = reconstruction_loss_dct
         loss["loss"] += reconstruction_loss_dct
         return loss
-        # x_dct = reconstruct_dct(x, k=self.k).to(self.device)
-        # reconstruction_loss = F.mse_loss(x_hat, x, reduction="sum") / x.shape[0]
-        # reconstruction_loss_dct = (
-        #    F.mse_loss(x_hat_dct, x_dct, reduction="sum") / x_dct.shape[0]
-        # )
-
-        # kl_divergence_z1 = 0.5 * (
-        #    delta_mu_1**2 / torch.exp(log_var_1)
-        #    + torch.exp(delta_log_var_1)
-        #    - delta_log_var_1
-        #    - 1
-        # ).sum(-1)
-
-        # kl_divergence_z2 = 0.5 * (
-        #    delta_mu_2**2 + torch.exp(delta_log_var_2) - delta_log_var_2 - 1
-        # ).sum(-1)
-
-        # kl_divergence = kl_divergence_z1.mean() + kl_divergence_z2.mean()
-
-        # loss = reconstruction_loss + reconstruction_loss_dct + self.beta * kl_divergence
-
-        # return {
-        #    "loss": loss,
-        #    "reconstruction_loss": reconstruction_loss,
-        #    "reconstruction_loss_dct": reconstruction_loss_dct,
-        #    "kl_divergence": kl_divergence,
-        # }
