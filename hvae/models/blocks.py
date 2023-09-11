cb74fd3dd4d929465bb457c4e6a748017f505083
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-09-11 23:33:45 +0200

    Add additional conv layers

--- hvae/models/blocks.py
+++ hvae/models/blocks.py
@@ -20,8 +20,14 @@ class Encoder(nn.Module):
         channels = [in_channels] + channels
         module = [
             nn.Sequential(
-                # nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1),
-                # activation(),
+                nn.Conv2d(
+                    in_channels,
+                    in_channels,
+                    kernel_size=3,
+                    stride=1,
+                    padding='same',
+                ),
+                activation(),
                 nn.Conv2d(
                     in_channels,
                     out_channels,
@@ -62,8 +68,14 @@ class Decoder(nn.Module):
         super().__init__()
         modules = [
             nn.Sequential(
-                # nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1),
-                # activation(),
+                nn.Conv2d(
+                    in_channels,
+                    in_channels,
+                    kernel_size=3,
+                    stride=1,
+                    padding='same',
+                ),
+                activation(),
                 nn.ConvTranspose2d(
                     in_channels,
                     out_channels,
@@ -193,7 +205,7 @@ class ResDecoder(nn.Module):
     """
 
     def __init__(self):
-        super(Decoder, self).__init__()
+        super(ResDecoder, self).__init__()
         self.rb1 = ResBlock(64, 48, 2, 2, 0, "decode")  # 48 4 4
         self.rb2 = ResBlock(48, 48, 2, 2, 0, "decode")  # 48 8 8
         self.rb3 = ResBlock(48, 32, 3, 1, 1, "decode")  # 32 8 8
