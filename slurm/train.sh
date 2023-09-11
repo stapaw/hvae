ea6700eac22206b72336caca1457fecdba17c9ff
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-09-11 23:58:23 +0200

    Refactor whitespace, make hydra more verbose

--- slurm/train.sh
+++ slurm/train.sh
@@ -17,14 +17,13 @@ additional_args="$@"
 
 source $HOME/.bashrc
 source $WORK/virtualenvs/hvae/bin/activate
+export HYDRA_FULL_ERROR=1
 
 python -m pip install --upgrade pip
 python -m pip install -r $HOME/hvae/requirements.txt
 python -m pip install -e $HOME/hvae
 
-srun --gres=gpu:1 python $HOME/hvae/train.py \
-    hydra.output_subdir=$WORK/projects/hvae/hydra \
-    wandb.save_dir=$WORK/projects/hvae/wandb \
-    dataset.root=$WORK/datasets \
-    model=dct_hvae dataset.classes=[1] model.beta=0.3 model.lr=0.004 \
+srun --gres=gpu:1 python $HOME/hvae/train.py hydra.output_subdir=$WORK/projects/hvae/hydra \
+    wandb.save_dir=$WORK/projects/hvae/wandb dataset.root=$WORK/datasets \
+    model=dct_hvae model.beta=0.3 model.lr=0.004 \
     $additional_args
